bce040c583e9c11ea749a90ac7e894d4bb8124d3
KietlinskiA <adrian.kietlinski@interia.pl>
2021-06-09 12:40:10 +0200

    Initial commit

---
+++ src/test/java/pl/kietlinski/teai3/Teai3ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kietlinski.teai3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Teai3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
